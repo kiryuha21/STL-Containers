e653bb0b431a796871194ad4c62ba44b74be3581
kiryuha21 <kiryuha.randomnumbers@gmail.com>
2023-04-20 16:09:47 +0300

    insert and erase tests for set

--- src/tests/set_suite.cc
+++ src/tests/set_suite.cc
@@ -110,4 +110,58 @@ TEST(SetSuite, clear_non_empty_test) {
   ASSERT_TRUE(my_set.begin() == my_set.end());
 }
 
+TEST(SetSuite, insert_to_empty_test) {
+  Set<int> a;
+  auto res = a.insert(1);
+  ASSERT_TRUE(res.second);
+  ASSERT_EQ(*(res.first), 1);
+  ASSERT_TRUE(sets_equal(a, std::set{1}));
+}
+
+TEST(SetSuite, insert_to_non_empty_test) {
+  Set<int> a = {1};
+  auto res = a.insert(2);
+  ASSERT_TRUE(res.second);
+  ASSERT_EQ(*(res.first), 2);
+  ASSERT_TRUE(sets_equal(a, std::set{1, 2}));
+}
+
+TEST(SetSuite, fail_insert_test) {
+  Set<int> a = {1};
+  auto res = a.insert(1);
+  ASSERT_FALSE(res.second);
+  ASSERT_EQ(res.first, a.end());
+  ASSERT_TRUE(sets_equal(a, std::set{1}));
+}
+
+TEST(SetSuite, erase_empty_test) {
+  Set<int> a;
+  ASSERT_THROW(a.erase(a.begin()), std::out_of_range);
+}
+
+TEST(SetSuite, erase_start_test) {
+  Set<int> a = {1, 2};
+  a.erase(a.begin());
+
+  ASSERT_EQ(*a.begin(), 2);
+  ASSERT_TRUE(sets_equal(a, std::set<int>{2}));
+}
+
+TEST(SetSuite, erase_end_test) {
+  Set<int> a = {1, 2};
+  a.erase(a.end());
+
+  ASSERT_EQ(*a.begin(), 1);
+  ASSERT_TRUE(sets_equal(a, std::set<int>{1}));
+}
+
+TEST(SetSuite, erase_mid_test) {
+  Set<int> a = {1, 2, 3};
+  a.erase(++a.begin());
+
+  ASSERT_EQ(*a.begin(), 1);
+  ASSERT_EQ(*a.end(), 3);
+  ASSERT_TRUE(sets_equal(a, std::set<int>{1, 3}));
+}
+
 }  // namespace s21
